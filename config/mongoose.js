93ea668ae9b7b2d5ccf4434454750dfd7cf402e4
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-03-01 17:10:25 +0530

    final projects

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,15 +1,16 @@
+const express = require("express");
 const mongoose = require("mongoose");
-
-// connecting to database
-mongoose.connect("mongodb://localhost/TODO-APP");
-
-const db = mongoose.connection;
-
-// error
-db.on("error", (err) => {
-  console.log(err.message);
-});
-// running db
-db.once("open", () => {
-  console.log("Successfully connected");
-});
+const app = express();
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1:27017/TODO-APP"),
+  {
+    userNewUrlParser: true,
+    UseUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("successfully connected");
+    }
+  };
